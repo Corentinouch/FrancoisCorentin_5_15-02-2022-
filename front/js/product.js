85665fd0a06f0562dd576508145eb7389713e595
Corentinouch <79088605+Corentinouch@users.noreply.github.com>
2022-02-23 16:28:26 +0100

    Add files via upload

--- front/js/product.js
+++ front/js/product.js
@@ -5,9 +5,12 @@ console.log(url);
 const id = url.searchParams.get("id");
 console.log(id);
 
+//transforme chaine de caractère en objet Json dans la variable storage
 let storage = JSON.parse(localStorage.getItem("panier"));
 console.log(storage);
+//si storage cad objet Json est null alors l'objet Json est un objet vide
 let objJson = storage !== null ? storage : {};
+console.log(objJson,"objet json");
 
 
     fetch("http://localhost:3000/api/products/" + id).then(async (res) =>{
@@ -51,20 +54,31 @@ let objJson = storage !== null ? storage : {};
 function storageAll(){
     let nombre = document.querySelector("#quantity");
     let couleur = item.querySelector("#colors");
-    if(objJson[produit._id] !== undefined){
+
+    //objet json avec produit id est dejà défini
+    if (objJson[produit._id] !== undefined){
+        //si la couleur n'est pas défini il la crée 
         objJson[produit._id].color[couleur.value] === undefined ? objJson[produit._id].color[couleur.value] = nombre.value 
+        //sinon il ajpute la valeur à la valeur deja existante
             : objJson[produit._id].color[couleur.value] = parseInt(nombre.value) + parseInt(objJson[produit._id].color[couleur.value]);
-    }else{
+            
+    } else {
+    //s'il l'objet n'est pas défini il le crée de zéro
+        //l'objet avec cet Id est un objet
         objJson[produit._id] = {}
+        //le sous objet de la variable couleur est un objet
         objJson[produit._id].color = {}
+        //set nombre de la couleur du sous objet couleur
         objJson[produit._id].color[couleur.value] = nombre.value;
+        //set name price image description
         objJson[produit._id].name = produit.name;
         objJson[produit._id].price = produit.price;
         objJson[produit._id].imageUrl = produit.imageUrl;
         objJson[produit._id].description = produit.description;
     }
-        
+        //transforme objet Json en chaine de caractère
         let objLinea = JSON.stringify(objJson);
+        //envoie vers le localstorage
         localStorage.setItem("panier",objLinea);
 
 }
