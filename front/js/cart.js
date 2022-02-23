cc2a8c7089b0a698b9a595d61c4c391e34dfd0ab
Corentinouch <79088605+Corentinouch@users.noreply.github.com>
2022-02-23 15:51:19 +0100

    Add files via upload

---
+++ front/js/cart.js
@@ -0,0 +1,63 @@
+let storage = JSON.parse(localStorage.getItem('panier'));
+console.log(storage);
+
+Object.keys(storage).map((key) => {
+    console.log(storage[key])
+    Object.keys(storage[key].color).map((k) => {
+        console.log(k,storage[key].color[k]);
+    })
+})
+
+
+let cards = document.getElementById('cart__items');
+console.log(cards);
+cards.innerHTML += Object.keys(storage).map((key) => {
+    console.log()
+    return Object.keys(storage[key].color).map((k) => {
+        return `<article class="cart__item" data-id="${key}" data-color="${k}">
+                    <div class="cart__item__img">
+                        <img src="${storage[key].imageUrl}" alt="Photographie d'un canapé">
+                    </div>
+                    <div class="cart__item__content">
+                        <div class="cart__item__content__description">
+                            <h2>${storage[key].name}</h2>
+                            <p>${k}</p>
+                            <p>${storage[key].price} €</p>
+                        </div>
+                        <div class="cart__item__content__settings">
+                            <div class="cart__item__content__settings__quantity">
+                            <p>Qté : </p>
+                            <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+                            <!--<div>${k} : ${storage[key].color[k]}</div>-->
+                            </div>
+                            <div class="cart__item__content__settings__delete">
+                            <p class="deleteItem">Supprimer</p>
+                            </div>
+                        </div>
+                    </div>
+                </article>`;
+            }).join('');
+        }).join('');
+
+
+    /*`<article class="cart__item" data-id="{product-ID}" data-color="{product-color}">
+                <div class="cart__item__img">
+                  <img src="../images/product01.jpg" alt="Photographie d'un canapé">
+                </div>
+                <div class="cart__item__content">
+                  <div class="cart__item__content__description">
+                    <h2>Nom du produit</h2>
+                    <p>Vert</p>
+                    <p>42,00 €</p>
+                  </div>
+                  <div class="cart__item__content__settings">
+                    <div class="cart__item__content__settings__quantity">
+                      <p>Qté : </p>
+                      <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="42">
+                    </div>
+                    <div class="cart__item__content__settings__delete">
+                      <p class="deleteItem">Supprimer</p>
+                    </div>
+                  </div>
+                </div>
+              </article>`;*/
--- front/js/product.js
+++ front/js/product.js
@@ -5,45 +5,73 @@ console.log(url);
 const id = url.searchParams.get("id");
 console.log(id);
 
+let storage = JSON.parse(localStorage.getItem("panier"));
+console.log(storage);
+let objJson = storage !== null ? storage : {};
 
-fetch("http://localhost:3000/api/products/" + id).then(async (res) =>{
-    const produit = await res.json();
-    console.log(produit);
 
-let item = document.querySelector(".item");
-    item.querySelector(".item__img").innerHTML +=
-    `<img src="${produit.imageUrl}" alt=${produit.altTxt}>
-    `;
+    fetch("http://localhost:3000/api/products/" + id).then(async (res) =>{
+        const produit = await res.json();
+        console.log(produit);
 
-    item.querySelector("#title").innerHTML +=
-    `<h1 class="productName">${produit.name}</h1>
-    `;
+//Remplacement des valeurs dans le .html 
 
-    item.querySelector("#price").innerHTML +=
-    `<span id="price">${produit.price}</span>
-    `;
+    let item = document.querySelector(".item");
 
-    item.querySelector("#description").innerHTML +=
-    `<p id="description">${produit.description}</p>
-    `;
+        item.querySelector(".item__img").innerHTML +=
+        `<img src="${produit.imageUrl}" alt=${produit.altTxt}>
+        `;
 
-let colors = produit.colors;
-console.log(colors);
+        item.querySelector("#title").innerHTML +=
+        `<h1 class="productName">${produit.name}</h1>
+        `;
 
-for (let i = 0; i < colors.length; i++) {
-    const color = colors[i];
-    console.log(color);
-    item.querySelector("#colors").innerHTML +=
-        `<option value="${produit.colors[i]}">${produit.colors[i]}</option>`;
+        item.querySelector("#price").innerHTML +=
+        `<span id="price">${produit.price}</span>
+        `;
+
+        item.querySelector("#description").innerHTML +=
+        `<p id="description">${produit.description}</p>
+        `;
+
+//Menu déroulent des couleurs de chaque produit
+
+    let colors = produit.colors;
+    console.log(colors);
+
+    for (let i = 0; i < colors.length; i++) {
+        const color = colors[i];
+        console.log(color);
+        item.querySelector("#colors").innerHTML +=
+            `<option value="${produit.colors[i]}">${produit.colors[i]}</option>`;
+    }
+    
+//Storage au moment du click sur ajout
+
+function storageAll(){
+    let nombre = document.querySelector("#quantity");
+    let couleur = item.querySelector("#colors");
+    if(objJson[produit._id] !== undefined){
+        objJson[produit._id].color[couleur.value] === undefined ? objJson[produit._id].color[couleur.value] = nombre.value 
+            : objJson[produit._id].color[couleur.value] = parseInt(nombre.value) + parseInt(objJson[produit._id].color[couleur.value]);
+    }else{
+        objJson[produit._id] = {}
+        objJson[produit._id].color = {}
+        objJson[produit._id].color[couleur.value] = nombre.value;
+        objJson[produit._id].name = produit.name;
+        objJson[produit._id].price = produit.price;
+        objJson[produit._id].imageUrl = produit.imageUrl;
+        objJson[produit._id].description = produit.description;
+    }
+        
+        let objLinea = JSON.stringify(objJson);
+        localStorage.setItem("panier",objLinea);
+
 }
-
-let objJson = {
-    id : produit._id,
-    color : produit.colors,
-    nombre : 170
-}
-let objLinea = JSON.stringify(objJson);
-localStorage.setItem("obj",objLinea);
+    
+    let addItem = document.getElementById('addToCart');
+    addItem.addEventListener('click', function(){
+        storageAll();
+    });
 
 });
-
